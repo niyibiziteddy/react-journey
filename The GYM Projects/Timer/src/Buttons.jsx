5d8c900e1f52bdb61b1f8be2aaefed7e734df28d
Teddy Niyibizi <niyibiziteddy@gmail.com>
2026-09-21 09:00:48 +0200

    Done with timer starting logic of timer

--- The GYM Projects/Timer/src/Buttons.jsx
+++ The GYM Projects/Timer/src/Buttons.jsx
@@ -1,9 +1,9 @@
-export default function Buttons({handleStart}){
+export default function Buttons({handleStart,state,reset,yesStart}){
     return(
         <>
             <div className="buttons">
-                <button className="buttons-item" onClick={handleStart}>Start</button>
-                <button className="buttons-item">Reset</button>
+                <button className="buttons-item" onClick={handleStart}>{!state || yesStart ? "Start" : "Stop"}</button>
+                <button className="buttons-item" onClick={reset}>Reset</button>
             </div>
         </>
     )
--- The GYM Projects/Timer/src/Counter.jsx
+++ The GYM Projects/Timer/src/Counter.jsx
@@ -1,33 +1,56 @@
 import Screen from "./Screen"
-import { useEffect,useState } from "react"
+import { useEffect,useState,useRef } from "react"
 import Buttons from "./Buttons"
 export default function Counter(){
 
-    const [sec,setSec] = useState(4);
-    const [min,setMin] = useState(2);
-    const [hour,setHour] = useState(1);
+    const [sec,setSec] = useState(5);
+    const [min,setMin] = useState(1);
+    const [hour,setHour] = useState(0);
     const [start,setStart] = useState(false);
-
-    const handleStart = (prev) => {setStart(!prev)};
+    let intervalId = useRef(null)
     
-    if(sec === 0 && min !== 0){
+    let yesStart = false
+    function reset(){
+        setMin(0)
+        setSec(0)
+        setHour(0)
+    }
+    if(min === 0 && hour !==0){
+        setHour(hour-1)
+        setMin(1)
+    }
+    else if(sec === 0 && min !== 0){
         setMin(min-1)
-        setSec(59)
+        setSec(10)
+    }
+    else if((hour === 0 && min === 0 && sec === 0) && start){
+        clearInterval(intervalId.current)
+        yesStart = true
     }
 
     useEffect(() => {
-        console.log('started')
-        let interval = setInterval(() => {
-            setSec((prev) => prev-1)
-        },1000)
-        return () => clearInterval(interval)
-    },[])
+        if(hour !== 0 || min !== 0 || sec !== 0){
+            console.log('Starting timer...')
+            if(start){
+            intervalId.current = setInterval(() => {
+            setSec((prev) => {
+                return prev - 1
+            })
+            },1000)
+            return () => clearInterval(intervalId.current)
+            }
+        }
+        else if((hour === 0 && min === 0 && sec === 0) && start){
+            setStart(false)
+        }
+        
+    },[start])
 
     return (
         <>
             <div className="container">
                 <Screen sec={`${sec}`} min={`${min}`} hour={`${hour}`}/>
-                <Buttons handleStart={handleStart}/>
+                <Buttons handleStart={() => setStart(!start)} yesStart={yesStart} state={start} reset ={reset}/>
             </div>
         </>
     )
